9ad433a920b84ae709bf5027b70e1a824bcc7713
jerpen80 <dra_penny@yahoo.com>
2024-10-10 15:34:59 +0200

    changed condition

--- variables.tf
+++ variables.tf
@@ -1,14 +1,6 @@
-variable "vpn_type" {
-  description = "Select VPN type: client_endpoint, site_2_site or both"
-  validation {
-    error_message = "Value can only be: \"client_endpoint\", \"site_2_site\" or \"both\"."
-    condition = contains(["client_endpoint", "site_2_site", "both"], var.vpn_type) 
-  }
-  
-}
-
 variable "client_endpoint_vpn" {
   description = "Configuration object indicating vpn endpoint setup."
+  default = null
   type = map(object({
       client_cidr_block : string
       target_cidr_block : string
@@ -19,6 +11,8 @@ variable "client_endpoint_vpn" {
 }
 
 variable "site_to_site_vpn" {
+  description = "Configuration object indicating site to site vpn setup."
+  default = null
   type = map(object({
       customer_ip : string
       type : string
