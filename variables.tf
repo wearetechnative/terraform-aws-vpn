3f54d35b6565716a8ced25e0c982c422fb191079
jerpen80 <dra_penny@yahoo.com>
2024-10-11 08:17:29 +0200

    added default value to parameter

--- variables.tf
+++ variables.tf
@@ -9,6 +9,7 @@ variable "vpn_type" {
 
 variable "client_endpoint_vpn" {
   description = "Configuration object indicating vpn endpoint setup."
+  default = ""
   type = map(object({
       client_cidr_block : string
       target_cidr_block : string
@@ -20,6 +21,7 @@ variable "client_endpoint_vpn" {
 
 variable "site_to_site_vpn" {
   description = "Configuration object indicating site to site vpn setup."
+  default = ""
   type = map(object({
       customer_ip : string
       type : string
